499520c5695bd38517aded0218fdc52c51cd636c
Sander Devisscher <sander.devisscher@inbo.be>
2024-02-03 20:29:12 +0100

    lcd test

--- src/main_code/main_code.ino
+++ src/main_code/main_code.ino
@@ -1,10 +1,14 @@
+#include <LiquidCrystal.h>
 int ledPin = 3;
 int knop;
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7); 
 
 void setup() {
   // put your setup code here, to run once:
  pinMode(ledPin, OUTPUT);
  pinMode(2, INPUT_PULLUP);
+ lcd.begin(16, 2); 
+ lcd.setCursor(0,0); 
 }
 
 void loop() {
@@ -12,9 +16,11 @@ void loop() {
 
   knop = digitalRead(2);
   if(knop == 0){
+   lcd.print("hello, world!"); 
    digitalWrite(ledPin, HIGH);
    delay(500);
    digitalWrite(ledPin, LOW);
    delay(500);
+   lcd.clear();
   }
 }
