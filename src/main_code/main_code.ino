540072c1f1bdc8371fb6674c9334e8ea3f4475a6
Sander Devisscher <sander.devisscher@inbo.be>
2024-02-03 19:59:21 +0100

    Update main_code.ino

--- src/main_code/main_code.ino
+++ src/main_code/main_code.ino
@@ -1,14 +1,20 @@
 int ledPin = 3;
+int knop;
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(ledPin, OUTPUT);
+ pinMode(ledPin, OUTPUT);
+ pinMode(2, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(ledPin, HIGH);
-  delay(500);
-  digitalWrite(ledPin, LOW);
-  delay(500);
+
+  knop = digitalRead(2);
+  if(knop == 0){
+   digitalWrite(ledPin, HIGH);
+   delay(500);
+   digitalWrite(ledPin, LOW);
+   delay(500);
+  }
 }
